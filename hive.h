814240f67b9a9896f4bf2e7a1d8f5d928947f882
trumanzhao <16861430@qq.com>
2017-08-10 21:40:32 +0800

    修正相对路径的处理

--- hive.h
+++ hive.h
@@ -17,7 +17,8 @@ public:
     hive_app() { }
     ~hive_app() { }
     int get_version(lua_State* L);
-    time_t get_file_time(const char* file_name);
+    time_t get_file_time(const char* filename);
+	int get_full_path(lua_State* L);
     int64_t get_time_ms();
     int64_t get_time_ns();
     void sleep_ms(int ms);
