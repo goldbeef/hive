35d0fa7199208af3f8fa2331719c18b96cf1e7db
“trumanzhao” <trumanzhao@tencent.com>
2019-05-08 08:50:56 +0800

    删除过时代码

--- bin/test.lua
+++ bin/test.lua
@@ -2,15 +2,10 @@
 
 require("base/log");
 require("base/tree");
-lbus = require("lbus");
-
-mgr = mgr or lbus.create_socket_mgr(1000);
 
 ct = ct or 0;
 
 function hive.run()
-    mgr.wait(1000);
-
     print("ct="..ct);
 	log_debug("ct=%d", ct);
     ct = ct + 1;
